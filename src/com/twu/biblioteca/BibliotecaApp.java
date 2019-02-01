9ac7404cc2e148b85a3c217d3e6184ec7dcdae15
Ella Jennings <erj503@gmail.com>
2019-02-01 14:36:48 -0800

    1.3 View Author and Publication Year on all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,17 +1,19 @@
 package com.twu.biblioteca;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class BibliotecaApp {
-    private static List<String> listOfBooks = Arrays.asList("Dark Places", "Talent Is Overrated", "Factfulness");
+    static String[] book1 = {"Dark Places", "Gillian", "Flynn", "2011"};
+    static String[] book2 = {"Talent Is Overrated", "Geoff", "Colvin", "2008"};
+    static String[] book3 = {"Factfulness", "Hans", "Rosling", "2018"};
 
 
     public static void main(String[] args) {
 
+        String[][] listOfBooks = {book1, book2, book3};
+
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-        for (String book: listOfBooks) {
-            System.out.println(book);
+        for (String[] book: listOfBooks) {
+            Book newBook = new Book(book[0], book[1], book[2], book[3]);
+            System.out.println(newBook.getBookInformation());
         }
     }
 
--- test/com/twu/biblioteca/BibliotecaAppTests.java
+++ test/com/twu/biblioteca/BibliotecaAppTests.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -17,9 +15,6 @@ import static org.junit.Assert.assertThat;
 public class BibliotecaAppTests {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     String[] book1 = {"Dark Places", "Gillian", "Flynn", "2011"};
-    String[] book2 = {"Talent Is Overrated", "Geoff", "Colvin", "2008"};
-    String[] book3 = {"Factfulness", "Hans", "Rosling", "2018"};
-    private List<String[]> booksList = Arrays.asList(book1, book2, book3);
 
     @Before
     public void setUpStreams() {
@@ -38,8 +33,9 @@ public class BibliotecaAppTests {
 
         assertEquals(4, output.length);
         assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", output[0]);
-        assertEquals("Dark Places", output[1]);
-        assertEquals("The Couple Next Door", output[7]);
+        assertEquals("Dark Places | Flynn, G | 2011", output[1]);
+        assertEquals("Talent Is Overrated | Colvin, G | 2008", output[2]);
+        assertEquals("Factfulness | Rosling, H | 2018", output[3]);
     }
 
     @Test

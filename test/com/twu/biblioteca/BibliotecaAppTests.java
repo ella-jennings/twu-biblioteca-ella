a97a871e47ba33a9d9161025e7e4b5d438544dd1
Ella Jennings <erj503@gmail.com>
2019-01-31 17:31:22 -0800

    1.1 View a welcome message

---
+++ test/com/twu/biblioteca/BibliotecaAppTests.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTests {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void StartingApplicationShouldDisplayWelcomeMessage() {
+        new BibliotecaApp().main(null);
+
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", outContent.toString());
+    }
+}

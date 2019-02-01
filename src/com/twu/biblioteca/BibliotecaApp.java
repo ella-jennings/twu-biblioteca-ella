cda8fe6f8134075a8d42312b05405b3ccd839b0c
Ella Jennings <erj503@gmail.com>
2019-01-31 18:27:51 -0800

    1.2 View a list of all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,18 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BibliotecaApp {
+    private static List<String> listOfBooks = Arrays.asList("Dark Places", "Talent Is Overrated", "Factfulness", "Slow Horses", "Sweet Little Lies", "In Order To Live", "The Couple Next Door");
+
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        for (String book: listOfBooks) {
+            System.out.println(book);
+        }
     }
 
 }
--- test/com/twu/biblioteca/BibliotecaAppTests.java
+++ test/com/twu/biblioteca/BibliotecaAppTests.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,5 +30,16 @@ public class BibliotecaAppTests {
         new BibliotecaApp().main(null);
 
         assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", outContent.toString());
+    }
+
+    @Test
+    public void StartingApplicationShouldDisplayListOfBooks() {
+        List<String> listOfBooks = Arrays.asList("Dark Places", "Talent Is Overrated", "Factfulness", "Slow Horses", "Sweet Little Lies", "In Order To Live", "The Couple Next Door");
+        new BibliotecaApp().main(null);
+        String output[] = outContent.toString().split("\n");
+
+        assertEquals(8, output.length);
+        assertEquals("Dark Places", output[1]);
+        assertEquals("The Couple Next Door", output[7]);
     }
 }

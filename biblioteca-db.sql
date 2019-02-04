bb5f5cd9da6dc7dd18372e1fdd691b11e6695f22
Ella Jennings <erj503@gmail.com>
2019-02-03 22:56:16 -0600

    Update biblioteca-db.sql

--- biblioteca-db.sql
+++ biblioteca-db.sql
@@ -21,5 +21,31 @@ WHERE member.id NOT IN (
 );
 
 -- 3. What books and movies aren't checked out?
+-- (There must be a more elegant way to achieve this?)
+-- 1984
+-- Catcher in the Rye
+-- Crouching Tiger, Hidden Dragon
+-- Domain Driven Design
+-- Fellowship of the Ring
+-- Lawrence of Arabia
+-- Office Space
+-- Thin Red Line
+-- To Kill a Mockingbird
+-- Tom Sawyer
+
+SELECT book.title 
+FROM book 
+WHERE NOT EXISTS 
+(SELECT * 
+FROM checkout_item 
+WHERE checkout_item.book_id = book.id)
+UNION
+SELECT movie.title
+FROM movie
+WHERE NOT EXISTS
+(SELECT * 
+FROM checkout_item 
+WHERE checkout_item.movie_id = movie.id);
+
 -- 4. Add the book 'The Pragmatic Programmer', and add yourself as a member. Check out 'The Pragmatic Programmer'. Use your query from question 1 to verify that you have checked it out. Also, provide the SQL used to update the database.
 -- 5. Who has checked out more that 1 item? 
